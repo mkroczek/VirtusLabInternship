dfbe469f6eed516eee48921451967100d54de5d3
mkroczek <1marcin100@gmail.com>
2022-04-16 00:16:44 +0200

    Pass ReceiptGeneratorTest

---
+++ src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
@@ -0,0 +1,26 @@
+package com.virtuslab.internship.receipt;
+
+import com.virtuslab.internship.basket.Basket;
+import com.virtuslab.internship.product.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ReceiptGenerator {
+
+    public Receipt generate(Basket basket) {
+
+        // count each product occurrences
+        Map<Product, Long> occurrences = basket.getProducts().stream()
+                .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+
+        // create receiptEntry for each product
+        List<ReceiptEntry> receiptEntries = new ArrayList<>(occurrences.keySet().stream()
+                .map(key -> new ReceiptEntry(key, Math.toIntExact(occurrences.get(key))))
+                .toList());
+
+        return new Receipt(receiptEntries);
+    }
+}
